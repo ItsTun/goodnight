32326d70391ec5324c2eb067537bd992a2dcaede
Tun Han <tunhanmra@rebbiz.com>
2023-04-03 19:06:21 +0630

    Add validations, associations, methods to SleepRecord Model

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,2 +1,15 @@
 class SleepRecord < ApplicationRecord
+  # Validations
+  validates :user_id, presence: true
+  validates :clock_in, presence: true
+  validate :clock_in_cannot_be_later_than_clock_out
+
+  # Associations
+  belongs_to :user
+
+  private
+
+  def clock_in_cannot_be_later_than_clock_out
+    errors.add(:clock_in, 'cannot be later than clock out') if clock_in && clock_out && clock_in > clock_out
+  end
 end
