01ed8e1dca2ee7d428ceef83ed431da9a7419f08
Tun Han <tunhanmra@rebbiz.com>
2023-04-03 19:49:02 +0630

    Refactor duplicate actions in users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,17 +2,26 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def follow
-    follower = User.find(params[:follower_id])
-    followed = User.find(params[:followed_id])
-
     follower.follow(followed)
-    render json: { message: 'Successfully followed user' }
+    render_success('followed')
   end
 
   def unfollow
-    follower = User.find(params[:follower_id])
-    followed = User.find(params[:followed_id])
     follower.unfollow(followed)
-    render json: { message: 'Successfully unfollowed user' }
+    render_success('unfollowed')
+  end
+
+  private
+
+  def follower
+    @follower ||= User.find(params[:follower_id])
+  end
+
+  def followed
+    @followed ||= User.find(params[:followed_id])
+  end
+
+  def render_success(action)
+    render json: { message: "Successfully #{action} user" }
   end
 end
