6b662ca570decd156cb742e49b4a398a3f251b71
Tun Han <tunhanmra@rebbiz.com>
2023-04-03 19:06:44 +0630

    Add FactoryBot SleepRecord

--- spec/factories/sleep_records.rb
+++ spec/factories/sleep_records.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :sleep_record do
-    
+    user
+    clock_in { Faker::Time.between(from: 2.days.ago, to: Time.now) }
+    clock_out { Faker::Time.between(from: clock_in, to: Time.now) }
   end
 end
