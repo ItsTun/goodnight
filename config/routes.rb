ae02c0d8eb74df7d35f5fbd7ee1f5803bf3a9a69
Tun Han <tunhanmra@rebbiz.com>
2023-04-03 19:10:00 +0630

    Add required routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  scope 'api/v1', defaults: { format: :json } do
+    # sleep_records
+    post 'clock_in', to: 'api/v1/sleep_records#clock_in'
+    get 'sleep_records', to: 'api/v1/sleep_records#index'
+    get 'friends_sleep_records', to: 'api/v1/sleep_records#friends_sleep_records'
+
+    # users
+    post 'follow', to: 'api/v1/users#follow'
+    post 'unfollow', to: 'api/v1/users#unfollow'
+  end
 end
