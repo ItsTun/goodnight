7321769830874b5f9d37580f514af52ac38e197a
Tun Han <tunhanmra@rebbiz.com>
2023-04-03 19:02:49 +0630

    Add validations, associations, methods to UserFollow Model

--- app/models/user_follow.rb
+++ app/models/user_follow.rb
@@ -1,2 +1,16 @@
 class UserFollow < ApplicationRecord
+  # Validations
+  validates :follower_id, presence: true, numericality: { only_integer: true }
+  validates :followed_id, presence: true, numericality: { only_integer: true }
+  validate :cannot_follow_self
+
+  # Associations
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+
+  private
+
+  def cannot_follow_self
+    errors.add(:followed_id, "can't be the same as follower_id") if follower_id == followed_id
+  end
 end
