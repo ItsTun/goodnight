9a34b5e13fc1c49003da65414c26e4caca057172
Tun Han <tunhanmra@rebbiz.com>
2023-04-04 14:47:26 +0630

    Add 1.week.ago.to_i to cache_key for sleep_records#friends_sleep_records

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -19,12 +19,10 @@ class Api::V1::SleepRecordsController < ApplicationController
   end
 
   def friends_sleep_records
-    @cache_key ||= begin
-      friends = User.find(params[:user_id]).followed_users
-      [friends.cache_key, 'sleep_records'].join('/')
-    end
-    @sleep_records = Rails.cache.fetch(@cache_key) do
-      SleepRecord.where(user_id: friends.ids)
+    friends = User.find(params[:user_id]).followed_users
+    cache_key = [friends.cache_key, 'sleep_records', 1.week.ago.to_i].join('/')
+    @sleep_records = Rails.cache.fetch(cache_key) do
+      SleepRecord.where(user_id: friends.select(:id))
                  .where('clock_in >= ?', 1.week.ago)
                  .order(Arel.sql('clock_out - clock_in DESC'))
     end
